4683b64406fe2f35dd32831049b02eeeb9a027b3
suryx7 <surendrapuri784995@gmail.com>
2024-07-30 19:08:57 +0530

    files updated

--- cal.js
+++ cal.js
@@ -1,19 +1,36 @@
 const display = document.getElementById("display");
+const display2 = document.getElementById("display2");
+
 
 function appendtoDisplay(input){
+    π = 3.14159265358979;
     display.value += input;
+}
+
+function cleared(){
+    display.value = display.value.slice(0, -1);
 }
 
 function clearDisplay(){ 
     display.value = "";
+    display2.value = "";
+}
+
+function power(){
+    display.value = String(Math.pow(display.value, 2));
+}
+
+function sqpower(){
+    display.value = String(Math.pow(display.value, 0.5));
 }
 
 function calculate(){
     try{
-        display.value = eval(display.value);
+        display2.value = eval(display.value);
+        // display.value = String(display2.value)
     }
     catch(error){
         display.value = "Error";
     }
     
-}
+} 
